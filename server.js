88fcef948933214ca496b0fe96c234ebfc122870
Suntgr <suntgr@163.com>
2018-06-19 19:01:54 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const puppeteer = require('puppeteer');
+const devices = require('puppeteer/DeviceDescriptors');
+const sizeOf = require('image-size');
+let baseUrl = process.argv[2];
+let baseStorageDir = process.argv[3];
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    userDataDir: __dirname + '/cache'
+  });
+  console.time('goto');
+  const page = await browser.newPage();
+  await page.emulate(devices['iPhone 6']);
+  console.timeEnd('goto');
+  app.get('/screenshot', async (req, res) => {
+    console.log(req.query.page)
+    try {
+      await page.goto(decodeURIComponent(req.query.page));
+      await page.screenshot({path: `${baseStorageDir}/${req.query.filename}.png`, fullPage: true});
+      let dimensions = await sizeOf(`${baseStorageDir}/${req.query.filename}.png`);
+      res.status(200).send({
+        url: `${baseUrl}/${req.query.filename}.png`,
+        width: dimensions.width,
+        height: dimensions.height
+      });
+    } catch (err) {
+      res.status(500).send({msg: '生成图片失败'});
+    }
+  })
+})();
+app.listen(3000, function () {
+  console.log('listening on port 3000')
+})
+
+
+
+
